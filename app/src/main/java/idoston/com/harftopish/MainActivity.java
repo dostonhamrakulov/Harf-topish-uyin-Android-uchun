2c7fc321891e409019a546466ef185d5e555f613
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-02-18 16:06:45 +0200

    Basic logic is working

--- app/src/main/java/idoston/com/harftopish/MainActivity.java
+++ app/src/main/java/idoston/com/harftopish/MainActivity.java
@@ -1,7 +1,10 @@
 package idoston.com.harftopish;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 /*
  *   Created by Doston Hamrakulov
  */
@@ -11,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Start_game(View view){
+        startActivity(new Intent(MainActivity.this, StartGame.class));
     }
 }
--- app/src/main/java/idoston/com/harftopish/StartGame.java
+++ app/src/main/java/idoston/com/harftopish/StartGame.java
@@ -2,17 +2,28 @@ package idoston.com.harftopish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.Random;
+
 /**
  * Created by Doston on 2/18/2018.
  */
 public class StartGame extends AppCompatActivity {
 
     ImageView image;
-    CheckBox ani_1, ani_2, ani_3, ani_4;
+    RadioGroup radioGroup;
+    RadioButton ani_1, ani_2, ani_3, ani_4;
     Button btn;
+    int random_question;
+    int random_position;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +31,86 @@ public class StartGame extends AppCompatActivity {
 
         image = (ImageView)findViewById(R.id.id_image);
 
-        ani_1 = (CheckBox) findViewById(R.id.id_ani_1);
-        ani_2 = (CheckBox) findViewById(R.id.id_ani_2);
-        ani_3 = (CheckBox) findViewById(R.id.id_ani_3);
-        ani_4 = (CheckBox) findViewById(R.id.id_ani_4);
+        radioGroup = (RadioGroup) findViewById(R.id.id_radio_g);
+        ani_1 = (RadioButton) findViewById(R.id.id_ani_1);
+        ani_2 = (RadioButton) findViewById(R.id.id_ani_2);
+        ani_3 = (RadioButton) findViewById(R.id.id_ani_3);
+        ani_4 = (RadioButton) findViewById(R.id.id_ani_4);
         btn = (Button) findViewById(R.id.id_btn_check);
 
+        ChangeQuestion();
+    }
+    public void Check(View view){
 
+        switch(view.getId()){
+            case R.id.id_ani_1:
+                if (ani_1.getText().toString() == Animals.Animal[random_question]){
+                    Toast.makeText(StartGame.this, "Correct", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(StartGame.this, "Incorrect", Toast.LENGTH_LONG).show();
+                }
+                ChangeQuestion();
+                break;
+            case R.id.id_ani_2:
+                if (ani_2.getText().toString() == Animals.Animal[random_question]){
+                    Toast.makeText(StartGame.this, "Correct", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(StartGame.this, "Incorrect", Toast.LENGTH_LONG).show();
+                }
+                ChangeQuestion();
+                break;
+            case R.id.id_ani_3:
+                if (ani_3.getText().toString() == Animals.Animal[random_question]){
+                    Toast.makeText(StartGame.this, "Correct", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(StartGame.this, "Incorrect", Toast.LENGTH_LONG).show();
+                }
+                ChangeQuestion();
+                break;
+            case R.id.id_ani_4:
+                if (ani_4.getText().toString() == Animals.Animal[random_question]){
+                    Toast.makeText(StartGame.this, "Correct", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(StartGame.this, "Incorrect", Toast.LENGTH_LONG).show();
+                }
+                ChangeQuestion();
+                break;
+        }
+
+    }
+    private void ChangeQuestion(){
+        random_position = getRandomForPosition();
+        random_question = getRandomForAnimal();
+
+        if(random_position == 0){
+            ani_1.setText(Animals.Animal[random_question]);
+            ani_2.setText(Animals.Animal[getRandomForAnimal()]);
+            ani_3.setText(Animals.Animal[getRandomForAnimal()]);
+            ani_4.setText(Animals.Animal[getRandomForAnimal()]);
+        }  else if ( random_position == 1){
+            ani_1.setText(Animals.Animal[getRandomForAnimal()]);
+            ani_2.setText(Animals.Animal[random_question]);
+            ani_3.setText(Animals.Animal[getRandomForAnimal()]);
+            ani_4.setText(Animals.Animal[getRandomForAnimal()]);
+        } else if ( random_position == 2){
+            ani_1.setText(Animals.Animal[getRandomForAnimal()]);
+            ani_2.setText(Animals.Animal[getRandomForAnimal()]);
+            ani_3.setText(Animals.Animal[random_question]);
+            ani_4.setText(Animals.Animal[getRandomForAnimal()]);
+        } else if (random_position == 3){
+            ani_1.setText(Animals.Animal[getRandomForAnimal()]);
+            ani_2.setText(Animals.Animal[getRandomForAnimal()]);
+            ani_3.setText(Animals.Animal[getRandomForAnimal()]);
+            ani_4.setText(Animals.Animal[random_question]);
+        }
+
+    }
+    private int getRandomForAnimal(){
+        Random random = new Random();
+        return random.nextInt(34);
+    }
+    private int getRandomForPosition(){
+        Random random = new Random();
+        return random.nextInt(4);
     }
 }
