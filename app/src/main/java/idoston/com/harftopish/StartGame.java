cebaeb9ac35a2aeb1578d95deebd743940a7c873
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-02-18 16:38:39 +0200

    Non-functional

--- app/src/main/java/idoston/com/harftopish/StartGame.java
+++ app/src/main/java/idoston/com/harftopish/StartGame.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -37,7 +36,6 @@ public class StartGame extends AppCompatActivity {
         ani_2 = (RadioButton) findViewById(R.id.id_ani_2);
         ani_3 = (RadioButton) findViewById(R.id.id_ani_3);
         ani_4 = (RadioButton) findViewById(R.id.id_ani_4);
-        btn = (Button) findViewById(R.id.id_btn_check);
 
         ChangeQuestion();
     }
