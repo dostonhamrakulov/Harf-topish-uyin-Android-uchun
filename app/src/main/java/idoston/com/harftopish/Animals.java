90cf1e773873fd0f2f64d46e9aff7c977c48b672
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-02-18 15:24:21 +0200

    Creation od Animals

---
+++ app/src/main/java/idoston/com/harftopish/Animals.java
@@ -0,0 +1,12 @@
+package idoston.com.harftopish;
+
+/**
+ * Created by Doston on 2/18/2018.
+ */
+
+public class Animals {
+
+    public static final String Animal[] = {"Ant", "Bear", "Beaver", "Bee", "Bird", "Bug", "Bull", "Butterfly", "Cat", "Caterpillar",
+            "Chicken", "Cow", "Deer", "Dinosaur", "Dog", "Dolphin", "Duck", "Elephant", "Fish", "Fly", "Frog", "Horse",  "Kangaroo",
+            "Lion", "Panda", "Pig", "Rabbit", "Seahorse", "Sheep", "Spider", "Stork", "Turtle", "Whale", "Wolf"};
+}
--- app/src/main/java/idoston/com/harftopish/StartGame.java
+++ app/src/main/java/idoston/com/harftopish/StartGame.java
@@ -2,12 +2,30 @@ package idoston.com.harftopish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+/**
+ * Created by Doston on 2/18/2018.
+ */
 public class StartGame extends AppCompatActivity {
 
+    ImageView image;
+    CheckBox ani_1, ani_2, ani_3, ani_4;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
+
+        image = (ImageView)findViewById(R.id.id_image);
+
+        ani_1 = (CheckBox) findViewById(R.id.id_ani_1);
+        ani_2 = (CheckBox) findViewById(R.id.id_ani_2);
+        ani_3 = (CheckBox) findViewById(R.id.id_ani_3);
+        ani_4 = (CheckBox) findViewById(R.id.id_ani_4);
+        btn = (Button) findViewById(R.id.id_btn_check);
+
+
     }
 }
