3c9a231c70d41b93e8c6fdd2610906721891f564
Doston Hamrakulov <doston2509@gmail.com>
2018-02-24 12:17:40 +0200

    some back-end

--- app/src/main/java/idoston/com/harftopish/countries.java
+++ app/src/main/java/idoston/com/harftopish/countries.java
@@ -1,7 +1,11 @@
 package idoston.com.harftopish;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class countries extends AppCompatActivity {
 
@@ -9,5 +13,35 @@ public class countries extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_countries);
+    }
+    public void Finish_m(View view){
+        Exit_alert();
+    }
+    public void Check_m(View view){
+
+    }
+    public void Exit_alert(){
+        AlertDialog.Builder alert_builder = new AlertDialog.Builder(this);
+        alert_builder.setMessage("O`yinni tugatishni xoxlaysizmi?")
+                .setCancelable(false)
+                .setPositiveButton("Ha", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(countries.this, "Xayr!", Toast.LENGTH_SHORT).show();
+                        moveTaskToBack(true);
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                        System.exit(1);
+                    }
+                })
+                .setNegativeButton("Yo`q", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+        AlertDialog alert = alert_builder.create();
+        alert.setTitle("Tasdiqlang");
+        alert.show();
     }
 }
