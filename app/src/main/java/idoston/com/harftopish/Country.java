eeb956c33f6747e5fc2f65821ead6d0f8795fac9
Doston Hamrakulov <doston2509@gmail.com>
2018-02-24 13:46:45 +0200

    Rasmlar

--- app/src/main/java/idoston/com/harftopish/Country.java
+++ app/src/main/java/idoston/com/harftopish/Country.java
@@ -5,5 +5,28 @@ package idoston.com.harftopish;
  */
 
 public class Country {
-    public static final String Animal[] = {};
+    public static final String country_image[] = {
+                    "af", "ad", "au", "ar", "am", "at",
+                    "az",  "bd", "by", "be", "br", "bg",
+                    "cn", "cz", "dk", "eg", "ee", "fi",
+                    "fr", "ge", "de", "gr", "hu", "in",
+                    "iq", "ie", "il", "it", "jp", "kz",
+                    "kg", "lv", "li", "lt", "lu", "my",
+                    "mc", "np", "nl", "nz", "ng", "kp",
+                    "no", "ca", "pk", "pl", "pt", "qa",
+                    "ro", "ru", "sg", "sk", "kr", "se",
+                    "sy", "uz", "tw", "tj", "th", "tr",
+                    "tm", "ua", "uz", "uz", "ve", "vn"}; //6*11 = 66
+    public static final String country_name[] = {
+            "Afghanistan",  "Andorra", "Australia",  "Argentina", "Armenia",
+            "Austria",      "Azerbaijan", "Bangladesh", "Belarus", "Belgium", "Brazil", "Bulgaria",
+            "China",        "Czech Republic", "Denmark", "Egypt", "Estonia", "Finland",
+            "France",       "Georgia", "Germany", "Greece", "Hungary", "India",
+            "Iraq",         "Ireland", "Israel", "Italy", "Japan", "Kazakhstan",
+            "Kyrgyzstan",    "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malaysia",
+            "Monaco",       "Nepal", "Netherlands", "New Zealand", "Nigeria", "North Korea",
+            "Norway",       "Canada", "Pakistan", "Poland", "Portugal", "Qatar",
+            "Romania",      "Russia", "Singapore", "Slovakia", "South Korea", "Sweden",
+            "Syria",        "Uzbekistan", "Taiwan", "Tajikistan", "Thailand", "Turkey",
+            "Turkmenistan", "Ukraine", "Uzbekistan", "United States of America", "Venezuela", "Vietnam"}; //11*6 = 66
 }
